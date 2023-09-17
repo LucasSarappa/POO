3a0552d068e25ee6d8315e4c5afbc31ed7e307c4
Lucas <lsarappa@frba.utn.edu.ar>
2023-09-17 19:19:29 -0300

    Subo POO

---
+++ src/Logica/Alumno.java
@@ -0,0 +1,23 @@
+package Logica;
+
+public class Alumno {
+    int id;
+    String nombre;
+    String apellido;
+
+    public Alumno() {
+    }
+
+    public Alumno(int id, String nombre, String apellido) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+
+    public void mostrarNombre(){
+        System.out.println("Hola, soy un alumno y se decir mi nombre");
+    }
+
+
+}
---
+++ src/Logica/POO.java
@@ -0,0 +1,11 @@
+// mi main
+
+package Logica;
+
+public class POO {
+
+    public static void main (String[],args){
+        Alumno alu1 = new Alumno(); //creo un objeto de tipo alumno vacio (usando el constructor vacio)
+        Alumno alu2 = new Alumno(5, "Juan", "Perez"); //creo un objeto de tipo alumno con los datos que le paso (usando el constructor con parametros)
+    }
+}
